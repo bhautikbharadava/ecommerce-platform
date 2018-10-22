a83d040c4a7ad00ef7c64ece88508c93aee66e1a
Bhautik Bharadava <bhautikbharadava@gmail.com>
2018-10-23 02:34:40 +0530

    Basic Navigation with next/link

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,12 @@
-import React from "react";
+import Link from "next/link";
 
-const Home = props => <div>Hey !</div>;
+const Home = props => (
+  <div>
+    <p>Hey !</p>
+    <Link href="/sell">
+      <a>Sell</a>
+    </Link>
+  </div>
+);
 
 export default Home;
