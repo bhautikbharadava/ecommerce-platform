9fb77b8372e932da93a7548ac0acf735d6755b05
Bhautik Bharadava <bhautikbharadava@gmail.com>
2019-01-27 00:13:53 +0530

    [ADDED] : Styled component in Header and theme

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -6,18 +6,18 @@ const theme = {
   red: "#FF0000",
   black: "#393939",
   gray: "#3a3a3a",
-  lightgrey: "#e1e1e1",
+  lightGrey: "#e1e1e1",
   offWhite: "#ededed",
   maxWidth: "1000px",
   bs: "0 12px 24px 0 rgba(0,0,0,0.09)"
 };
 const StyledPage = styled.div`
   background: white;
-  color: black;
+  color: ${props => props.theme.black};
 `;
 
 const Inner = styled.div`
-  max-width: 1000px;
+  max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
   padding: 2rem;
 `;
