d18789585765c8bffffc32a5c565078afd1fd001
Bhautik Bharadava <bhautikbharadava@gmail.com>
2019-01-27 16:20:27 +0530

    [ADDED] : Global styles

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -16,6 +16,33 @@ const StyledPage = styled.div`
   color: ${props => props.theme.black};
 `;
 
+injectGlobal`
+  @font-face {
+    font-family:'radnika-next' ;
+    src: url('/static/radnikanext-medium-webfont.woff2')format('woff2');
+    font-weight:normal;
+    font-style:normal;
+  }
+  html{
+    box-sizing:border-box;
+    font-size:10px;
+  }
+  *,*:after,*:before{
+    box-sizing:inherit;
+  }
+  body{
+    padding:0;
+    margin:0;
+    font-size:1.5rem;
+    line-height:2;
+    font-family:'radnika-next'
+  }
+  a{
+    text-decoration:none;
+    color :${theme.black}
+  }
+`;
+
 const Inner = styled.div`
   max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
